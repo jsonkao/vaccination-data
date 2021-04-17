bb508dd0fc5c3ceb769e649a0c1302fe37e906dd
Jason Kao <jason.kao@columbia.edu>
2021-04-17 17:30:21 -0400

    r stuff

--- bloomberg/aggregate.py
+++ bloomberg/aggregate.py
@@ -2,15 +2,26 @@ import json
 import sys
 import os
 
-DIR = 'bloomberg'
+DIR = "bloomberg"
 
-print('date,state,race_known,ethnicity_known')
+print("date,state,race_known,ethnicity_known")
 
 for fname in os.listdir(DIR):
-    if '.json' not in fname:
+    if ".json" not in fname:
         continue
-    with open(DIR + '/' + fname) as f:
-        data = json.load(f)['demographics'][3:]
-    date = fname.split('.')[0]
+    with open(DIR + "/" + fname) as f:
+        data = json.load(f)["demographics"][3:]
+    date = fname.split(".")[0]
     for state in data:
-        print(date + ',' + ','.join(state[k] for k in ['name', 'demoRaceDataQuality', 'demoEthnicityDataQuality']))
+        print(
+            date
+            + ","
+            + ",".join(
+                state[k]
+                for k in [
+                    "name",
+                    "demoRaceDataQuality",
+                    "demoEthnicityDataQuality"
+                ]
+            )
+        )
